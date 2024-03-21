e9de7b250d515fd41be452ae8f0bceba4009f7cc
pochetnyi-vasilyi <41404596+pochetnyi-vasilyi@users.noreply.github.com>
2024-03-22 01:49:57 +0300

    Add ref_link config.py

--- data/config.py
+++ data/config.py
@@ -2,8 +2,7 @@
 DELAY = (5, 20)
 
 # web3go рефка
-# REF_LINK = 'https://reiki.web3go.xyz?ref=52306fd993dd4e74'
-REF_LINK = 'https://reiki.web3go.xyz'
+REF_LINK = 'https://reiki.web3go.xyz?ref=11d3a0a38c30a03f'
 
 # bnb rpc
 BNB_RPC = 'https://rpc.ankr.com/bsc'
