742efdcb8286370af96e1c2c7bf548d571f277ec
pochetnyi-vasilyi <41404596+pochetnyi-vasilyi@users.noreply.github.com>
2024-03-22 01:47:19 +0300

    Initial commit

---
+++ data/config.py
@@ -0,0 +1,9 @@
+# задержка в секундах
+DELAY = (5, 20)
+
+# web3go рефка
+# REF_LINK = 'https://reiki.web3go.xyz?ref=52306fd993dd4e74'
+REF_LINK = 'https://reiki.web3go.xyz'
+
+# bnb rpc
+BNB_RPC = 'https://rpc.ankr.com/bsc'
